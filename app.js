19a33106573d8236438b07d219aa51d1fd74a722
huiju <gmlwn6316@gmail.com>
2020-07-20 05:06:04 +0900

    web design change
    web design change
    join, login, upload, uploadQuestion,home

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
+app.use(express.static(__dirname+'/'));
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
